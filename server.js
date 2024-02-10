328c0aafb3ad0b8810f841518c09485c838b3a0b
Martin <moothreene@gmail.com>
2024-02-10 21:58:04 +0300

    auth serverside

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ const mongoose = require('mongoose');
 const User = require("./models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const salt = bcrypt.genSaltSync(10);
 const secret = "ashkhkshatosjkv";
 
 app.use(cors({credentials:true,origin:"http://localhost:3000"}));
 app.use(express.json());
+app.use(cookieParser());
+
 mongoose.connect("mongodb+srv://WowItemDataApi:kogqJtQAjaI0UkDb@wowitemdata.ttre1pw.mongodb.net/WowItemData");
 
 app.get("/api", async (req, res)=>{
@@ -53,22 +56,34 @@ app.post("/register", async (req,res)=>{
 });
 
 app.post("/login", async (req,res)=>{
-
   const {username,password} = req.body;
   const userDoc = await User.findOne({username});
   const passCorrect = bcrypt.compareSync(password,userDoc.password);
-  console.log(passCorrect);
   if(passCorrect){
     jwt.sign({username, id:userDoc._id},secret,{},(err,token)=>{
       if(err) throw(err);
-      res.cookie("token",token).json("ok");
+      res.cookie("token",token).json({
+        id:userDoc._id,
+        username,
+      });
       }
     );
-    res.json
   }else{
     res.status(400).json("Wrong Username or Password");
   }
-})
+});
+
+app.get("/profile", (req,res)=>{
+  const {token} = req.cookies;
+  jwt.verify(token,secret,{},(error,data)=>{
+    if(error) throw error
+    res.json(data);
+  });
+});
+
+app.post("/logout", (req,res)=>{
+  res.cookie("token","").json("ok");
+});
 
 app.listen(PORT,()=>{
     console.log(`running on port ${PORT}`);
