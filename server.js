b33775b00393f8d03a99d246c6a211896046a428
Martin <moothreene@gmail.com>
2024-02-10 03:34:43 +0300

    adding new requests for register and login

--- server.js
+++ server.js
@@ -4,8 +4,17 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const database = require('./database');
 const fs = require('fs');
+const mongoose = require('mongoose');
+const User = require("./models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-app.use(cors());
+const salt = bcrypt.genSaltSync(10);
+const secret = "ashkhkshatosjkv";
+
+app.use(cors({credentials:true,origin:"http://localhost:3000"}));
+app.use(express.json());
+mongoose.connect("mongodb+srv://WowItemDataApi:kogqJtQAjaI0UkDb@wowitemdata.ttre1pw.mongodb.net/WowItemData");
 
 app.get("/api", async (req, res)=>{
   try {
@@ -27,6 +36,39 @@ app.get("/img", async (req, res)=>{
       res.status(500).send("Server Error");
     }
 });
+
+app.post("/register", async (req,res)=>{
+  const {username, password} = req.body;
+  try{
+    const userDoc = await User.create(
+      {
+        username,
+        password:bcrypt.hashSync(password,salt)
+      }
+      );
+    res.json(userDoc);
+  }catch(error){
+    res.status(400).json(error);
+  }
+});
+
+app.post("/login", async (req,res)=>{
+
+  const {username,password} = req.body;
+  const userDoc = await User.findOne({username});
+  const passCorrect = bcrypt.compareSync(password,userDoc.password);
+  console.log(passCorrect);
+  if(passCorrect){
+    jwt.sign({username, id:userDoc._id},secret,{},(err,token)=>{
+      if(err) throw(err);
+      res.cookie("token",token).json("ok");
+      }
+    );
+    res.json
+  }else{
+    res.status(400).json("Wrong Username or Password");
+  }
+})
 
 app.listen(PORT,()=>{
     console.log(`running on port ${PORT}`);
