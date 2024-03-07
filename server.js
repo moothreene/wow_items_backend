a3270ea2082769b0b13050db2ce6190731123cc5
Martin <moothreene@gmail.com>
2024-03-07 18:29:16 +0300

    database url vulnerability removed

--- server.js
+++ server.js
@@ -23,8 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use("/images", express.static(__dirname+"/images"));
-
-mongoose.connect("mongodb+srv://WowItemDataApi:kogqJtQAjaI0UkDb@wowitemdata.ttre1pw.mongodb.net/WowItemData");
+mongoose.connect(process.env.DATABASE_URL);
 
 app.get("/api", async (req, res)=>{
   try {
